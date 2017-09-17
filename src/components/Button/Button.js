2a1602eddc532bf4afd9991208b21eaf98c9bc1a
Toni Sanchez <toninos@gmail.com>
2017-09-17 01:11:07 +0100

    Button new size property

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,20 +4,29 @@ import FontAwesome from 'react-fontawesome'
 import PropTypes from 'prop-types'
 
 const MyButton = (props) => {
+  let color = props.color
+  let buttonStyle
+  if(props.large) {
+    buttonStyle = [styles.button, styles.fullWidth].join(' ')
+  } else {
+    buttonStyle = [styles.button, styles.halfWidth].join(' ')
+  }
   return (
     <button
-      className={styles.button}
+      className={buttonStyle}
       id={props.id}
       value={props.value}
       onClick={props.handleOnChange}>
-      <span className={styles.button__text}>
+      <span
+        className={styles.button__text}>
         {props.text}
       </span>
       <span className={styles.button__icon}>
         <FontAwesome
+          value={props.value}
           name={props.icon}
           size='3x'
-          style={{ color: '#A06FEFs'}}
+          style={{ color: color }}
         />
       </span>
     </button>
@@ -26,8 +35,15 @@ const MyButton = (props) => {
 
 MyButton.propTypes = {
   id: PropTypes.string.isRequired,
+  color: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.bool
+  ]),
   icon: PropTypes.string.isRequired,
-  value: PropTypes.string.isRequired,
+  value: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.bool
+  ]),
   handleOnChange: PropTypes.func.isRequired
 }
 
