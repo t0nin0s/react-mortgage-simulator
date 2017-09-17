37a6f07eeec68adea11672eeef9eaed9dd9513b3
Toni Sanchez <toninos@gmail.com>
2017-09-17 23:36:28 +0100

    Adding validations

--- src/signupForm/SignupFormResident/SignupFormResident.js
+++ src/signupForm/SignupFormResident/SignupFormResident.js
@@ -1,45 +1,71 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Card from '../../components/Card/Card'
 import PropTypes from 'prop-types'
 import styles from '../signupForm.css'
 import FontAwesome from 'react-fontawesome'
 import MyButton from '../../components/Button/Button'
 
-const SignupFormResident = (props) => {
-  if(props.resident === 'Yes') { console.log('is resident') }
-  return (
-    <Card
-      formTitle='Sign up'
-      icon='home'>
-      <div className={styles.form__question}>
-        Are you a UK Resident?
-      </div>
-      <div className={styles.form__body}>
-        <MyButton
-          id='ukResident'
-          value='No'
-          text='No'
-          icon='times-circle'
-          color='red'
-          handleOnChange={props.handleOnChange} />
-        <MyButton
-          id='ukResident'
-          value='Yes'
-          text='Yes'
-          icon='check-circle'
-          color='green'
-          handleOnChange={props.handleOnChange} />
-      </div>
-      <div className={styles.form__submit}>
-        <FontAwesome
-          name='arrow-circle-right'
-          size='3x'
-          style={{ color: '#ECECEC' }}
-          onClick={props.handleNext}
-        />
-      </div>
-    </Card>
-  )
+class SignupFormResident extends Component {
+  constructor() {
+    super()
+    this.state = {
+      error: false
+    }
+  }
+
+  validate = () => {
+    if (this.props.resident) {
+      this.setState({
+        error: false
+      })
+      this.props.handleNext()
+    } else {
+      this.setState({
+        error: true
+      })
+    }
+  }
+
+  render() {
+    return (
+      <Card
+        formTitle='Sign up'
+        icon='home'>
+        <div className={styles.form__question}>
+          Are you a UK resident?
+        </div>
+        <div className={styles.form__body}>
+          <MyButton
+            id='ukResident'
+            value='No'
+            text='No'
+            icon='times-circle'
+            color='red'
+            handleOnChange={this.props.handleOnChange} />
+          <MyButton
+            id='ukResident'
+            value='Yes'
+            text='Yes'
+            icon='check-circle'
+            color='green'
+            handleOnChange={this.props.handleOnChange} />
+        </div>
+        { this.state.error &&
+          <div className={styles.error_message}>
+            Please select first your residency...
+          </div>
+        }
+        <div className={styles.form__submit}>
+          <FontAwesome
+            name='arrow-circle-right'
+            size='3x'
+            style={{ color: '#ECECEC' }}
+            onClick={this.validate}
+          />
+        </div>
+      </Card>
+    )
+  }
 }
 
 SignupFormResident.propTypes = {
--- src/signupForm/signupFormGender/SignupFormGender.js
+++ src/signupForm/signupFormGender/SignupFormGender.js
@@ -1,45 +1,72 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Card from '../../components/Card/Card'
 import PropTypes from 'prop-types'
 import styles from '../signupForm.css'
 import FontAwesome from 'react-fontawesome'
 import MyButton from '../../components/Button/Button'
 
-const SignupFormGender = (props) => {
-  return (
-    <Card
-      formTitle='Sign up'
-      icon='question-circle-o'
-      footer>
-      <div className={styles.form__question}>
-        Please tell us your gender...
-      </div>
-      <div className={styles.form__body}>
-        <MyButton
-          id='gender'
-          value='male'
-          text='Male'
-          icon='mars'
-          large
-          handleOnChange={props.handleOnChange} />
-        <MyButton
-          id='gender'
-          value='female'
-          text='Female'
-          icon='venus'
-          large
-          handleOnChange={props.handleOnChange} />
-      </div>
-      <div className={styles.form__submit}>
-        <FontAwesome
-          name='arrow-circle-right'
-          size='3x'
-          style={{ color: '#ECECEC' }}
-          onClick={props.handleNext}
-        />
-      </div>
-    </Card>
-  )
+class SignupFormGender extends Component {
+  constructor() {
+    super()
+    this.state = {
+      error: false
+    }
+  }
+
+  validate = () => {
+    if (this.props.gender) {
+      this.setState({
+        error: false
+      })
+      this.props.handleNext()
+    } else {
+      this.setState({
+        error: true
+      })
+    }
+  }
+
+  render() {
+    return (
+      <Card
+        formTitle='Sign up'
+        icon='question-circle-o'
+        footer>
+        <div className={styles.form__question}>
+          Please tell us your gender...
+        </div>
+        <div className={styles.form__body}>
+          <MyButton
+            id='gender'
+            value='male'
+            text='Male'
+            icon='mars'
+            large
+            handleOnChange={this.props.handleOnChange} />
+          <MyButton
+            id='gender'
+            value='female'
+            text='Female'
+            icon='venus'
+            large
+            handleOnChange={this.props.handleOnChange} />
+        </div>
+        { this.state.error &&
+          <div className={styles.error_message}>
+            Please select first your gender...
+          </div>
+        }
+        <div className={styles.form__submit}>
+          <FontAwesome
+            name='arrow-circle-right'
+            size='3x'
+            style={{ color: '#ECECEC' }}
+            onClick={this.validate}
+          />
+        </div>
+      </Card>
+    )
+  }
 }
 
 SignupFormGender.propTypes = {
