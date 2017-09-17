71c395d9c0916d92a05525483a017d90b889249f
Toni Sanchez <toninos@gmail.com>
2017-09-17 13:54:46 +0100

    Adding Validation personal data step

--- src/signupForm/signupFormPersonal/SignupFormPersonal.js
+++ src/signupForm/signupFormPersonal/SignupFormPersonal.js
@@ -9,10 +9,42 @@ import FontAwesome from 'react-fontawesome'
 import PropTypes from 'prop-types'
 
 class SignupFormPersonal extends Component {
+  constructor() {
+    super()
+    this.state = {
+      title: null,
+      name: null,
+      surname: null
+    }
+  }
 
   handleSubmit = (event) => {
-    console.log('submit')
     this.props.handleOnChange()
+  }
+
+  validate = () => {
+    let error = false
+    let inputs = {
+      title: this.props.title,
+      name: this.props.name,
+      surname: this.props.surname
+    }
+    Object.entries(inputs).forEach(([key, value]) => {
+      if(!value) {
+        this.setState({
+          [key]: 'error'
+        })
+        error = true
+      } else {
+        this.setState({
+          [key]: null
+        })
+      }
+    })
+
+    if(!error) {
+      this.props.handleNext()
+    }
   }
 
   render () {
@@ -25,7 +57,7 @@ class SignupFormPersonal extends Component {
           Please tell us your name...
         </div>
         <form className={styles.form__body} onSubmit={this.handleSubmit}>
-          <FormGroup>
+          <FormGroup validationState={this.state.title}>
             <FormControl
               value={this.props.title}
               onChange={this.props.handleOnChange}
@@ -38,6 +70,8 @@ class SignupFormPersonal extends Component {
               <option value='Mr'>Mr</option>
               <option value='Mrs'>Mrs</option>
             </FormControl>
+          </FormGroup>
+          <FormGroup validationState={this.state.name}>
             <FormControl
               value={this.props.name}
               onChange={this.props.handleOnChange}
@@ -46,6 +80,8 @@ class SignupFormPersonal extends Component {
               type='string'
               placeholder='First Name'
             />
+          </FormGroup>
+          <FormGroup validationState={this.state.surname}>
             <FormControl
               value={this.props.surname}
               onChange={this.props.handleOnChange}
@@ -60,7 +96,7 @@ class SignupFormPersonal extends Component {
               name='arrow-circle-right'
               size='3x'
               style={{ color: '#ECECEC'}}
-              onClick={this.props.handleNext }
+              onClick={this.validate}
             />
           </div>
         </form>
